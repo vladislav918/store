1a6bef17226e410c93c2962ea86a8e66fe658357
vladislav918 <lvlal5759@gmail.com>
2023-08-19 22:54:30 +0700

    fix github action

--- store/settings.py
+++ store/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),  
-        'PORT': os.getenv('DB_PORT'),  
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  
+        'PORT': 5432,  
     }
 }
 
