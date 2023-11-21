889f7ea84d2723fba7ff2ac2c5e29e629e77ebc1
vladislav918 <lvlal5759@gmail.com>
2023-11-21 20:32:07 +0700

    fix cart

--- cart/cart.py
+++ cart/cart.py
@@ -12,7 +12,7 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
         self.coupon_id = self.session.get('coupon_id')
-
+    
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
@@ -22,6 +22,8 @@ class Cart:
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
+            print(cart.values())
+            print(item)
             yield item
 
     def __len__(self):
@@ -39,7 +41,6 @@ class Cart:
         self.save()
 
     def save(self):
-        self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
 
     def remove(self, product):
@@ -66,7 +67,8 @@ class Cart:
 
     def get_discount(self):
         if self.coupon:
-            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+            return (self.coupon.discount / Decimal(100)) \
+                * self.get_total_price()
         return Decimal(0)
 
     def get_total_price_after_discount(self):
